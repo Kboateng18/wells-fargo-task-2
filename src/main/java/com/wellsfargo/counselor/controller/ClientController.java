7ee2e7fc773e2b449aac5576ed9f5ce88ab49bb9
Kwaku Boateng <boatengkwaku1963@gmail.com>
2025-01-19 04:01:57 -0600

    Added these to check and validate whether web session was running

---
+++ src/main/java/com/wellsfargo/counselor/controller/ClientController.java
@@ -0,0 +1,43 @@
+package com.wellsfargo.counselor.controller;
+
+import com.wellsfargo.counselor.entity.Client;
+import com.wellsfargo.counselor.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<List<Client>> getAllClients() {
+        return ResponseEntity.ok(clientService.getAllClients());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClientById(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.getClientById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> createClient(@RequestBody Client client) {
+        return ResponseEntity.ok(clientService.createClient(client));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client) {
+        return ResponseEntity.ok(clientService.updateClient(id, client));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        clientService.deleteClient(id);
+        return ResponseEntity.ok().build();
+    }
+}
